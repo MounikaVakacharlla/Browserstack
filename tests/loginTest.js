99585251ce0ff4c10f1288b996abd75569aaec24
Mounikavakacharlla <vakacharlamounika@gmail.com>
2026-05-28 19:29:44 +0530

    fixed some issues

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -1,21 +1,27 @@
-const { Builder, By, until } = require("selenium-webdriver");
+const { Builder } = require("selenium-webdriver");
 const assert = require("assert");
 
 describe("Google Test", function () {
-  this.timeout(30000);
+
+  this.timeout(120000);
 
   let driver;
 
   before(async function () {
-    driver = await new Builder().forBrowser("chrome").build();
+    driver = await new Builder()
+      .forBrowser("chrome")
+      .build();
   });
 
   it("Open Google and verify title", async function () {
+
     await driver.get("https://www.google.com");
 
     const title = await driver.getTitle();
 
-    assert.strictEqual(title.includes("Google"), true);
+    console.log(title);
+
+    assert(title.includes("Google"));
   });
 
   after(async function () {
